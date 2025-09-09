4ebaf4e31f6c643d31cae78e2840fc2cf064bbdf
Fede <fedeboccazzi@gmail.com>
2025-09-09 17:55:32 -0300

    creo orden de pizza veggie y pepperoni

--- abstract_factory/main.py
+++ abstract_factory/main.py
@@ -4,6 +4,8 @@ def main():
     ny = NYPizzaStore(); chi = ChicagoPizzaStore()
     ny.order_pizza("cheese")
     chi.order_pizza("clam")
+    ny.order_pizza("veggie")
+    chi.order_pizza("pepperoni")
 
 if __name__ == "__main__":
     main()
