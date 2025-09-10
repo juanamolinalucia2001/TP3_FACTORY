e9373da06bf7e1a543b7f59ada14a07d210f0808
Federico Boccazzi <fedeboccazzi@gmail.com>
2025-09-10 09:37:01 -0300

    Cambio clase veggie por clases onion, mushroom, pepper y garlic. Agrego pizza veggie para Ny y Chi en main para visualizar prueba

--- abstract_factory/main.py
+++ abstract_factory/main.py
@@ -5,6 +5,7 @@ def main():
     ny.order_pizza("cheese")
     chi.order_pizza("clam")
     ny.order_pizza("veggie")
+    chi.order_pizza("veggie")
     chi.order_pizza("pepperoni")
 
 if __name__ == "__main__":
